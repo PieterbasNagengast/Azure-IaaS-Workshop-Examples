94eaa4a1a423f5cf03dc078ab4a7b7c0c62e2756
Pieterbas Nagengast <pieterbas.nagengast@microsoft.com>
2023-10-16 17:46:28 +0200

    fix secureboot

--- AzCLI/example-VirtualMachine.sh
+++ AzCLI/example-VirtualMachine.sh
@@ -10,7 +10,6 @@ az vm create \
     --image "MicrosoftWindowsServer:WindowsServer:2022-datacenter-g2:20348.1006.220908" \
     --public-ip-sku Standard \
     --admin-username $username \
-    --secure-boot enabled \
     --size Standard_B1s \
     --storage-sku StandardSSD_LRS \
     --security-type "TrustedLaunch" \
